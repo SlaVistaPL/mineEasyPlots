dc8002ee7684267d99a01371902f374dd04ac269
Nikox <niko123571@gmail.com>
2020-08-04 14:40:31 +0200

    Fix messages validation

--- src/main/java/pl/mineEasyPlots/configs/FileUpdater.java
+++ src/main/java/pl/mineEasyPlots/configs/FileUpdater.java
@@ -16,18 +16,18 @@ public class FileUpdater {
             Main.getInst().saveDefaultConfig();
             Logger logger = Main.getInst().getLogger();
             logger.log(Level.WARNING, "[Updater] Saved default config from jar.");
-            logger.log(Level.WARNING, "[Updater] Old config saved to " + old.getAbsolutePath() + ".");
+            logger.log(Level.WARNING, "[Updater] Old config saved to " + old.getAbsolutePath() + ".old.");
         }
     }
     // Version: 1.1
     public static void checkMessages(){
-        if(Messages.messages.get("reloadSuccess") == null){
+        if(Messages.messages.get("messages.reloadSuccess") == null){
             File old = new File(Main.getInst().getDataFolder(), "messages.yml");
             old.renameTo(new File(Main.getInst().getDataFolder(), "messages.yml.old"));
             Main.getInst().saveResource("messages.yml", false);
             Logger logger = Main.getInst().getLogger();
             logger.log(Level.WARNING, "[Updater] Saved default messages from jar.");
-            logger.log(Level.WARNING, "[Updater] Old messages saved to " + old.getAbsolutePath() + ".");
+            logger.log(Level.WARNING, "[Updater] Old messages saved to " + old.getAbsolutePath() + ".old.");
         }
     }
 }
